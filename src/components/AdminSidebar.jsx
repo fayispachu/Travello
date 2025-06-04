87fe6db75411b471d08da17419976f2c43ad5952
Fayis <fayizpachu217@gmail.com>
2025-06-04 10:55:39 +0530

    frontend done1

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -6,7 +6,7 @@ function AdminSidebar() {
       <div className="w-40 h-[100vh] flex flex-col  p-1 gap-1  ">
         <button className="bg-[#33D69F] py-2 mb-5">Dashboard</button>
         <button className="hover:bg-[#33D69F] bg-gray-100 py-2  ">
-          Total Users
+          Total Users     
         </button>
         <button className="hover:bg-[#33D69F] bg-gray-100 py-2">
           Recent Trips
